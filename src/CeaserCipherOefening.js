3424830a3fe419670d91d3a8671423ec98c358b3
Neeraj <r1008394@student.thomasmore.be>
2024-10-17 16:47:13 +0200

    CaeserCipher decryption section

--- src/CeaserCipherOefening.js
+++ src/CeaserCipherOefening.js
@@ -4,7 +4,7 @@ const letter = 'T'
 const offset = alphabet.indexOf(letter) + 1;
 const codeword = 'SREERAJ';
 
-
+//ENCRYPT
 function encode(message, codeword, offset){
     let encodedMessage = "";
     message = message.toUpperCase().replace(/ /g, '[');
@@ -30,6 +30,42 @@ function encode(message, codeword, offset){
 }
 
 
+//DECRYPT
+function decode(encodedMessage, codeword, offset) {
+    let decodedMessage = "";
+
+    for (let i = 0; i < encodedMessage.length; i++) {
+        const encodedChar = encodedMessage[i];
+        const encodedIndex = alphabet.indexOf(encodedChar) + 1;
+
+        const codeWord = codeword[i % codeword.length];
+        const codeWordIndex = alphabet.indexOf(codeWord) + 1;
+
+        let originalShift = (encodedIndex - codeWordIndex - offset);
+
+
+        if (originalShift <= 0) {
+            while (originalShift <= 0) {
+                originalShift += 27;
+            }
+        }
+        // originalShift = ((originalShift % 27) + 27) % 27;
+
+        if (encodedIndex === 27) {
+            originalShift = 27;
+        }
+
+        decodedMessage += alphabet[originalShift - 1];
+    }
+
+    return decodedMessage.replace(/\[/g, ' ');
+}
+
+
+// Example usage:
 const message = "ATTACK AT DAWN";
 const encodedMessage = encode(message, codeword, offset);
 console.log("Encoded Message:", encodedMessage);
+
+const decodedMessage = decode(encodedMessage, codeword, offset);
+console.log("Decoded Message:", decodedMessage); // Expected output: "ATTACK AT DAWN"
